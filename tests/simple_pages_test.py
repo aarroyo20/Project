8812708fe4f2c122bd932d339604f389ff0c8988
Angela Arroyo <ara55@njit.edu>
2022-03-21 23:28:44 -0400

    added and edited the new 4 article pages as part of project 2

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -8,6 +8,10 @@ def test_request_main_menu_links(client):
     assert b'<li class="nav-item"><a class="nav-link" href="/page2">Docker</a></li>' in response.data
     assert b'<li class="nav-item"><a class="nav-link" href="/page3">Python and Flask</a></li>' in response.data
     assert b'<li class="nav-item"><a class="nav-link" href="/page4">CICD</a></li>' in response.data
+    assert b'<li class="nav-item"><a class="nav-link" href="/article1">Python and others</a></li>' in response.data
+    assert b'<li class="nav-item"><a class="nav-link" href="/article2">AAA Testing</a></li>' in response.data
+    assert b'<li class="nav-item"><a class="nav-link" href="/article3">OOPs</a></li>' in response.data
+    assert b'<li class="nav-item"><a class="nav-link" href="/article4">SOLID</a></li>' in response.data
 
 def test_request_index(client):
     """This makes the index page"""
